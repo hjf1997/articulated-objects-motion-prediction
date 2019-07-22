87f5e5a527309db1224439ef58159278efb8a15f
p0werHu <278053122@qq.com>
2019-07-22 16:14:28 +0800

    update

--- src/STLN.py
+++ src/STLN.py
@@ -13,5 +13,15 @@ class STLN(nn.Module):
     def __init__(self):
         super().__init__()
 
+    def forward(self, encoder_inputs, decoder_inputs):
+        pass
+
+class STLNCell(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+
+
     def forward(self, *input):
         pass
